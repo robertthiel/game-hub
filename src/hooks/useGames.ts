3a806314b19332f270c96aa9af42c8a51822fb7d
Robert Thiel <robertthiel@yahoo.com>
2024-02-17 12:17:13 +0700

    show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -29,18 +29,25 @@ const useGames = () => {
 
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
   
     useEffect(() => {
 
 
-const controller = new AbortController()
+      const controller = new AbortController()
 
+      setLoading(true);
       apiClient
         .get<FetchGamesResponse>("/games", {signal: controller.signal})
-        .then((res) => setGames(res.data.results))
+        .then((res) => {
+          setGames(res.data.results)
+          setLoading(false);
+        })
         .catch((err) => {
             if (err instanceof CanceledError ) return;
-            setError(err.message)});
+            setError(err.message)
+          setLoading(false);
+          });
     
 
         return () => {
@@ -48,7 +55,7 @@ const controller = new AbortController()
         }
     }, [] );
 
-return { games, error}
+return { games, error, isLoading}
 
 }
 
